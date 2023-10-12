180d823a600baf8fc14cffcdd3151e20b7f24bf2
tupeo31072002@gmail.com <tupeo31072002@gmail.com>
2023-10-12 16:49:27 +0700

    add ci/cd pipeline

---
+++ processed_data/down.sh
@@ -0,0 +1,4 @@
+# wget -P ./processed_data https://post-reddit-dataset.s3.amazonaws.com/ml_reddit_node.npy
+# wget -P ./processed_data https://post-reddit-dataset.s3.amazonaws.com/ml_reddit.csv
+# wget -P ./processed_data https://post-reddit-dataset.s3.amazonaws.com/ml_reddit.npy
+# wget -P ./processed_data https://post-reddit-dataset.s3.amazonaws.com/reddit.csv
